4899fee9787f6e54d472b97287267b8908f15ac1
AlessioBona <alefbona@gmail.com>
2019-01-19 00:41:17 +0100

    prova

--- pohlwg_bot.py
+++ pohlwg_bot.py
@@ -180,10 +180,10 @@ def makeButtons(bot, update, args):
 
 def button2(bot, update):
     query = update.callback_query
-
-    bot.edit_message_text(text="Selected - option: {}".format(query.data),
-                          chat_id=query.message.chat_id,
-                          message_id=query.message.message_id)
+    update.message.reply_text("Selected - option: {}".format(query.data))
+    # bot.edit_message_text(text="Selected - option: {}".format(query.data),
+    #                       chat_id=query.message.chat_id,
+    #                       message_id=query.message.message_id)
     
 
 def main():
