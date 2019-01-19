2b1385fa466857a02c5495b33efbe2fe810837f9
selinaZitrone <selina.baldauf@gmx.de>
2019-01-19 14:13:25 +0100

    button function try to replace alpha_ with empty string when replying to pressed button

--- pohlwg_bot.py
+++ pohlwg_bot.py
@@ -149,7 +149,7 @@ def start(bot, update):
 def button(bot, update):
     query = update.callback_query
 
-    bot.edit_message_text(text="Selected option: {}".format(query.data),
+    bot.edit_message_text(text="Selected option: {}".format(query.data.replace("alpha_","")),
                           chat_id=query.message.chat_id,
                           message_id=query.message.message_id)
 
